a633d3c81d3ca3da0694096ae715cee58c69ad54
seanpen00 <seanpen00@gmail.com>
2021-12-10 01:34:21 -0500

    added

---
+++ src/components/state/state.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import "./state.css"
+import StateCard from "./stateCard";
+class State extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            data:[
+                {
+                    title:".1 ETH",
+                    text:"Price"
+                },
+                {
+                    title:"5K",
+                    text:"Collection Size"
+                },
+                {
+                    title:"53",
+                    text:"Total Traits"
+                },
+                {
+                    title:"--",
+                    text:"Trees Planted"
+                },
+                {
+                    title:"--",
+                    text:"$USD Donated"
+                }
+            ]
+        }
+    }
+    render(){
+        console.log(this.state.data);
+        return(
+            <div className="state_layout">
+
+                <p className="state_title">Days until Mint</p>
+                <img style={{paddingBottom: 20}}src="https://gen.sendtric.com/countdown/oqbi49z8p8"/>
+                <div className="state_cards">
+                    {
+                    this.state.data.map(element=>{
+                        return(
+                            <div
+                            className="state_card">
+                                <StateCard 
+                                title={element.title}
+                                text={element.text}/>
+                            </div>
+                            )
+                    })
+                }
+                </div>
+                <div className="state_bg">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+export default State;
